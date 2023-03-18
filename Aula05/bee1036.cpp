2392e28af7821873020ce876e0670ee89e532657
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-03-18 09:34:59 -0300

    18/03/2023

--- Aula05/bee1036.cpp
+++ Aula05/bee1036.cpp
@@ -1,25 +1,29 @@
 #include <stdio.h>
 #include <math.h>
-float calculoDelta(float ){
-    return (b * b) - 4 * a * c)
-}
-float bhaskaraPositivo(float num1, float num2, float delta)
+float calculoDelta(float A, float B, float C)
 {
-    return ((-num2 + t) / (2 * num1))
+    return (B * B) - 4.0 * A * C;
+}
+float bhaskaraPositivo(float num1, float num2, float num3)
+{
+    return ((-num2 + sqrt(calculoDelta(num1, num2, num3))) / (2 * num1));
+}
+float bhaskaraNegativo(float num1, float num2, float num3)
+{
+    return ((-num2 - sqrt(calculoDelta(num1, num2, num3))) / (2 * num1));
 }
 int main()
 {
-    double a, b, c, t;
+    double a, b, c;
     scanf("%lf %lf %lf", &a, &b, &c);
 
-    if (((b * b) - 4 * a * c) < 0 || a == 0)
+    if (calculoDelta(a, b, c) < 0 || a == 0)
     {
         printf("Impossivel calcular\n");
     }
     else
     {
-        t = sqrt((b * b) - 4 * a * c);
-        printf("R1 = %.5lf\nR2 = %.5lf\n", ((-b + t) / (2 * a)), ((-b - t) / (2 * a)));
+        printf("R1 = %.5lf\nR2 = %.5lf\n", bhaskaraPositivo(a, b, c), bhaskaraNegativo(a, b, c));
     }
     return 0;
 }
