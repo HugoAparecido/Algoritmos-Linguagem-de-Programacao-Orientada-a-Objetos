37c30f9b03cc3b4b159779fbdb81170eb20d8de4
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-05-13 14:45:07 -0300

    13/05/2023

--- Aula 12/Exercicio1.c
+++ Aula 12/Exercicio1.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int main(int argc, char *argv[])
+#include <string.h>
+int main()
 {
     struct Candidata
     {
@@ -11,29 +11,30 @@ int main(int argc, char *argv[])
         float altura;
         float peso;
         char naturalidade[50];
-        string estado;
-    } Candidata[] candidata = new Candidata[30];
-    for (int i = 0; i < candidata.Length; i++)
+        char estado[2];
+    };
+    struct Candidata candidata[30];
+    for (int i = 0; i < 30; i++)
     {
-        Console.Write("Digite o número da candidata: ");
-        candidata[i].numeroInscricao = Convert.ToInt32(Console.ReadLine());
-        Console.Write("Digite o nome da candidata: ");
-        candidata[i].nome = Console.ReadLine();
-        Console.Write("Digite a altura da candidata: ");
-        candidata[i].altura = (float)Convert.ToDouble(Console.ReadLine());
-        Console.Write("Digite o peso da candidata: ");
-        candidata[i].peso = (float)Convert.ToDouble(Console.ReadLine());
-        Console.Write("Digite a naturalidade da candidata: ");
-        candidata[i].naturalidade = Console.ReadLine();
-        Console.Write("Digite a sigla do estado da Candidata (exemplo: SP): ");
-        candidata[i].estado = Console.ReadLine();
+        printf("\nDigite o numero da candidata: ");
+        scanf("%i", &candidata[i].numeroInscricao);
+        printf("Digite o nome da candidata: ");
+        scanf("%s", &candidata[i].nome);
+        printf("Digite a altura da candidata: ");
+        scanf("%f", &candidata[i].altura);
+        printf("Digite o peso da candidata: ");
+        scanf("%f", &candidata[i].peso);
+        printf("Digite a naturalidade da candidata: ");
+        scanf("%s", candidata[i].naturalidade);
+        printf("Digite a sigla do estado da Candidata (exemplo: SP): ");
+        scanf("%s", candidata[i].estado);
     }
-    Console.WriteLine("As candidatas aprovadas foram: ");
-    for (int i = 0; i < candidata.Length; i++)
+    printf("\nAs candidatas aprovadas foram: ");
+    for (int i = 0; i < 30; i++)
     {
-        if (candidata[i].estado == "SP" && candidata[i].altura > 1.85)
+        if (strcmp("SP", candidata[i].estado) == 0 && candidata[i].altura > 1.85)
         {
-            Console.WriteLine(candidata[i].nome);
+            printf("\n%s", candidata[i].nome);
         }
     }
 }
