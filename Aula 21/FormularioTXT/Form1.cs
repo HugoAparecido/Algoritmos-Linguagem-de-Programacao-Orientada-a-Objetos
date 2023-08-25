1a398ca2b1fd680c1fd4ed926eade6607290ee94
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-08-25 09:25:16 -0300

    25/08/2023

---
+++ Aula 21/FormularioTXT/Form1.cs
@@ -0,0 +1,16 @@
+namespace FormularioTXT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter texto1;
+            string caminho = "D:\\Atividades de programação e desenvolvimento feitos no Instituto Federal\\Algoritmos-Linguagem-de-Programa--o-Orientada-a-Objetos\\Aula 21";
+        }
+    }
+}
