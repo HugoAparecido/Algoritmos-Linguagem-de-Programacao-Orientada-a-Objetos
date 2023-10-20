7df559ac95f7ee84a09af9360b6b4e2ae7d3357b
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-10-20 06:51:23 -0300

    20-10-2023

--- Aula 22/AppBanco/AppBanco/Form1.Designer.cs
+++ Aula 22/AppBanco/AppBanco/Form1.Designer.cs
@@ -42,13 +42,17 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(113, 277);
+            this.button1.Location = new System.Drawing.Point(120, 341);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(80, 50);
+            this.button1.Size = new System.Drawing.Size(107, 62);
             this.button1.TabIndex = 0;
             this.button1.Text = "Cadastrar";
             this.button1.UseVisualStyleBackColor = true;
@@ -56,9 +60,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(206, 277);
+            this.button2.Location = new System.Drawing.Point(465, 341);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 50);
+            this.button2.Size = new System.Drawing.Size(100, 62);
             this.button2.TabIndex = 1;
             this.button2.Text = "Pesquisar";
             this.button2.UseVisualStyleBackColor = true;
@@ -67,108 +72,156 @@
             // textBox1
             // 
             this.textBox1.Enabled = false;
-            this.textBox1.Location = new System.Drawing.Point(90, 104);
+            this.textBox1.Location = new System.Drawing.Point(120, 128);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(222, 20);
+            this.textBox1.Size = new System.Drawing.Size(295, 22);
             this.textBox1.TabIndex = 2;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(90, 130);
+            this.textBox2.Location = new System.Drawing.Point(120, 160);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(222, 20);
+            this.textBox2.Size = new System.Drawing.Size(295, 22);
             this.textBox2.TabIndex = 3;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(90, 159);
+            this.textBox3.Location = new System.Drawing.Point(120, 196);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(222, 20);
+            this.textBox3.Size = new System.Drawing.Size(295, 22);
             this.textBox3.TabIndex = 4;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(90, 194);
+            this.textBox4.Location = new System.Drawing.Point(120, 239);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(222, 20);
+            this.textBox4.Size = new System.Drawing.Size(295, 22);
             this.textBox4.TabIndex = 5;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(90, 232);
+            this.textBox5.Location = new System.Drawing.Point(120, 286);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(222, 20);
+            this.textBox5.Size = new System.Drawing.Size(295, 22);
             this.textBox5.TabIndex = 6;
             // 
             // listView1
             // 
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(318, 104);
+            this.listView1.Location = new System.Drawing.Point(424, 128);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.listView1.MultiSelect = false;
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(684, 223);
+            this.listView1.Size = new System.Drawing.Size(911, 180);
             this.listView1.TabIndex = 7;
             this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.ListView1_ItemSelectionChanged);
+            this.listView1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.ListView1_MouseClick);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(50, 108);
+            this.label1.Location = new System.Drawing.Point(67, 133);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.Size = new System.Drawing.Size(20, 16);
             this.label1.TabIndex = 8;
             this.label1.Text = "ID";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(36, 134);
+            this.label2.Location = new System.Drawing.Point(48, 165);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.Size = new System.Drawing.Size(44, 16);
             this.label2.TabIndex = 9;
             this.label2.Text = "Nome";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(41, 159);
+            this.label3.Location = new System.Drawing.Point(55, 196);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(27, 13);
+            this.label3.Size = new System.Drawing.Size(33, 16);
             this.label3.TabIndex = 10;
             this.label3.Text = "CPF";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(36, 194);
+            this.label4.Location = new System.Drawing.Point(48, 239);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.Size = new System.Drawing.Size(41, 16);
             this.label4.TabIndex = 11;
             this.label4.Text = "Email";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(19, 232);
+            this.label5.Location = new System.Drawing.Point(25, 286);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(49, 13);
+            this.label5.Size = new System.Drawing.Size(61, 16);
             this.label5.TabIndex = 12;
             this.label5.Text = "Telefone";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(315, 88);
+            this.label6.Location = new System.Drawing.Point(420, 108);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.Size = new System.Drawing.Size(65, 16);
             this.label6.TabIndex = 13;
             this.label6.Text = "Registros";
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(235, 341);
+            this.button3.Margin = new System.Windows.Forms.Padding(4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 62);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Atualizar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(350, 341);
+            this.button4.Margin = new System.Windows.Forms.Padding(4);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 62);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "Apagar";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(573, 361);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(295, 22);
+            this.textBox6.TabIndex = 16;
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(1014, 450);
+            this.ClientSize = new System.Drawing.Size(1352, 554);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -183,6 +236,7 @@
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -206,6 +260,9 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox6;
     }
 }
 
--- Aula 22/AppBanco/AppBanco/Form1.cs
+++ Aula 22/AppBanco/AppBanco/Form1.cs
@@ -13,6 +13,7 @@ namespace AppBanco
 {
     public partial class Form1 : Form
     {
+        private int ?id_selecionado = null;
         public Form1()
         {
             InitializeComponent();
@@ -25,7 +26,9 @@ namespace AppBanco
             listView1.Columns.Add("NOME", 200, HorizontalAlignment.Left);
             listView1.Columns.Add("CPF", 100, HorizontalAlignment.Left);
             listView1.Columns.Add("EMAIL", 200, HorizontalAlignment.Left);
-            listView1.Columns.Add("TELEFONE", 100, HorizontalAlignment.Left);
+            listView1.Columns.Add("TELEFONE", 100, HorizontalAlignment.Left); 
+            button3.Enabled = false;
+            button4.Enabled = false;
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -34,13 +37,15 @@ namespace AppBanco
             string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=banco;";
             string query = "INSERT INTO usuario(`id`, `nome`, `cpf`, `email`, `telefone`) VALUES (NULL, '" + textBox2.Text + "', '" + textBox3.Text + "', '" + textBox4.Text + "', '" + textBox5.Text + "')";
             MySqlConnection databaseConnection = new MySqlConnection(connectionString);
-            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
-            commandDatabase.CommandTimeout = 60;
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection)
+            {
+                CommandTimeout = 60
+            };
             try
             {
                 databaseConnection.Open();
                 MySqlDataReader myReader = commandDatabase.ExecuteReader();
-                MessageBox.Show("Usuário Cadstrado com sucesso!!");
+                MessageBox.Show("Usuário Cadastrado com sucesso!!");
                 databaseConnection.Close();
             }
             catch (Exception ex)
@@ -53,10 +58,13 @@ namespace AppBanco
         {
             //Botão Pesquisar
             string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=banco;";
-            string query = "SELECT * FROM usuario";
+            //string query = "SELECT * FROM usuario";
+            string query = "SELECT * FROM usuario WHERE nome LIKE '%" + textBox6.Text + "%'";
             MySqlConnection databaseConnection = new MySqlConnection(connectionString);
-            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
-            commandDatabase.CommandTimeout = 60;
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection)
+            {
+                CommandTimeout = 60
+            };
             MySqlDataReader reader;
             try
             {
@@ -82,6 +90,76 @@ namespace AppBanco
             {
                 MessageBox.Show(ex.Message);
             }
+        }
+
+        private void ListView1_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+            System.Windows.Forms.ListView.SelectedListViewItemCollection itens_selecionados = listView1.SelectedItems;
+            foreach (ListViewItem item in itens_selecionados)
+            {
+                id_selecionado = Convert.ToInt32(item.SubItems[0].Text);
+                textBox1.Text = item.SubItems[0].Text;
+                textBox2.Text = item.SubItems[1].Text;
+                textBox3.Text = item.SubItems[2].Text;
+                textBox4.Text = item.SubItems[3].Text;
+                textBox5.Text = item.SubItems[4].Text;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //Botão Atualizar
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=banco;";
+            string query = "UPDATE `usuario` SET `nome` = '" + textBox2.Text + "', `cpf` = '" + textBox3.Text + "', `email` = '" + textBox4.Text + "', `telefone` = '" + textBox5.Text + "' WHERE id = '" + textBox1.Text + "'";
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection)
+            {
+                CommandTimeout = 60
+            };
+            try
+            {
+                databaseConnection.Open();
+                MySqlDataReader myReader = commandDatabase.ExecuteReader();
+                MessageBox.Show("Usuário Atualizado com sucesso!!");
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //Botão Deletar-Apagar
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=banco;";
+            string query = "DELETE FROM `usuario` WHERE id = '" + textBox1.Text + "'";
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection)
+            {
+                CommandTimeout = 60
+            };
+            try
+            {
+                DialogResult res = MessageBox.Show("Tem certeza que deseja excluir o registro?", "Absoluta?", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                if (res == DialogResult.Yes)
+                {
+                    databaseConnection.Open();
+                    MySqlDataReader myReader = commandDatabase.ExecuteReader();
+                    MessageBox.Show("Usuário Removido com sucesso!!");
+                }
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void ListView1_MouseClick(object sender, MouseEventArgs e)
+        {
+            textBox3.Enabled = true;
+            textBox4.Enabled = true;
         }
     }
 }
