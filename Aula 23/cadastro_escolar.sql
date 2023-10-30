6bcca109a5e4148de9c08eba565042d7e92111e6
HugoAparecido <120059026+HugoAparecido@users.noreply.github.com>
2023-10-30 16:28:12 -0300

    30/10/2023

--- Aula 23/cadastro_escolar.sql
+++ Aula 23/cadastro_escolar.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Tempo de geração: 06-Out-2023 às 12:49
+-- Tempo de geração: 22-Out-2023 às 17:05
 -- Versão do servidor: 8.0.31
 -- versão do PHP: 8.0.26
 
@@ -44,6 +44,15 @@ CREATE TABLE IF NOT EXISTS `aluno` (
   PRIMARY KEY (`Ra`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
+--
+-- Extraindo dados da tabela `aluno`
+--
+
+INSERT INTO `aluno` (`Ra`, `nome`, `cpf`, `endereco`, `telefone`, `email`, `curso`, `nota_Bim1`, `nota_Bim2`, `nota_Bim3`, `nota_Bim4`, `media`) VALUES
+('16543789080-98', 'Andrus Neves Romam', '904.786.908-90', 'andrus.neves@gmail.com', 'Informática', '(19)78964-9024', 'Rua Praça Nova 98', 10, 9, 8, 2, 7),
+('78965431989-13', 'Júlia Eduarda Carvalho', '876.345.908-12', 'Administração', 'juju.dudu@gmai', 'Rua Alexandre Fleming 747', '(18)09456-3420', 7, 9, 10, 5, 7),
+('56723987156-90', 'Hugo Aparecido Gregorio de Almeida', '245.908.564-34', 'Rua Alexandre Fleming 747', '(18)90876-2387', 'hugoapga626@gmail.com', 'Informática', 10, 10, 10, 8, 9);
+
 -- --------------------------------------------------------
 
 --
@@ -57,7 +66,17 @@ CREATE TABLE IF NOT EXISTS `curso` (
   `cargaHoraria` int NOT NULL,
   `periodo` varchar(25) NOT NULL,
   PRIMARY KEY (`codigo`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Extraindo dados da tabela `curso`
+--
+
+INSERT INTO `curso` (`codigo`, `nome`, `cargaHoraria`, `periodo`) VALUES
+(1, 'Administração', 100, 'Diurno'),
+(2, 'Informática', 200, 'Integral'),
+(3, 'Engenharia da Computação', 140, 'Integral'),
+(4, 'Licenciatura em Matemática', 140, 'Integral');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
---
+++ Aula 24/CadAlunoCurso/Form2.cs
@@ -0,0 +1,109 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Windows.Forms;
+
+namespace CadAlunoCurso
+{
+    public partial class FormCadastrarAluno : Form
+    {
+        readonly FormPesquisa formPesquisaLocal;
+        readonly System.Windows.Forms.ListView.SelectedListViewItemCollection selecionado;
+        public FormCadastrarAluno(FormPesquisa formPesquisa, System.Windows.Forms.ListView.SelectedListViewItemCollection itens_selecionados)
+        {
+            InitializeComponent();
+            formPesquisaLocal = formPesquisa;
+            selecionado = itens_selecionados;
+            if (selecionado != null)
+            {
+                foreach (ListViewItem item in selecionado)
+                {
+                    caixaInsercaoRA.Text = item.SubItems[0].Text;
+                    caixaInsercaoNome.Text = item.SubItems[1].Text;
+                    caixaInsercaoCPF.Text = item.SubItems[2].Text;
+                    escolherCurso.Text = item.SubItems[3].Text;
+                    caixaInsercaoEmail.Text = item.SubItems[4].Text;
+                    caixaInsercaoEndereco.Text = item.SubItems[5].Text;
+                    caixaInsercaoTelefone.Text = item.SubItems[6].Text;
+                    caixaInsercaoNotaBim1.Text = item.SubItems[7].Text;
+                    caixaInsercaoNotaBim2.Text = item.SubItems[8].Text;
+                    caixaInsercaoNotaBim3.Text = item.SubItems[9].Text;
+                    caixaInsercaoNotaBim4.Text = item.SubItems[10].Text;
+                    caixaMostrarMedia.Text = item.SubItems[11].Text;
+                    caixaInsercaoRA.Enabled = false;
+                }
+            }
+        }
+
+        private void PesquisarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            formPesquisaLocal.ShowDialog();
+        }
+
+        private void AlunoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Refresh();
+        }
+
+        private void CursoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            FormCadastroCurso formCadastroCurso = new FormCadastroCurso(formPesquisaLocal, null);
+            formCadastroCurso.ShowDialog();
+        }
+
+        private void CadastarAluno_Click(object sender, EventArgs e)
+        {
+            FormPesquisa.Aluno aluno = new FormPesquisa.Aluno(caixaInsercaoRA.Text, caixaInsercaoNome.Text, caixaInsercaoCPF.Text, Convert.ToInt32(escolherCurso.SelectedIndex), caixaInsercaoEmail.Text, caixaInsercaoEndereco.Text, caixaInsercaoTelefone.Text, Convert.ToDouble(caixaInsercaoNotaBim1.Text), Convert.ToDouble(caixaInsercaoNotaBim2.Text), Convert.ToDouble(caixaInsercaoNotaBim3.Text), Convert.ToDouble(caixaInsercaoNotaBim4.Text));
+            aluno.Cadastrar();
+        }
+
+        private void AtualizarAluno_Click(object sender, EventArgs e)
+        {
+            FormPesquisa.Aluno aluno = new FormPesquisa.Aluno(caixaInsercaoRA.Text, caixaInsercaoNome.Text, caixaInsercaoCPF.Text, Convert.ToInt32(escolherCurso.SelectedIndex), caixaInsercaoEmail.Text, caixaInsercaoEndereco.Text, caixaInsercaoTelefone.Text, Convert.ToDouble(caixaInsercaoNotaBim1.Text), Convert.ToDouble(caixaInsercaoNotaBim2.Text), Convert.ToDouble(caixaInsercaoNotaBim3.Text), Convert.ToDouble(caixaInsercaoNotaBim4.Text));
+            aluno.Atualizar();
+        }
+
+        private void DeletarAluno_Click(object sender, EventArgs e)
+        {
+            FormPesquisa.Aluno aluno = new FormPesquisa.Aluno(caixaInsercaoRA.Text, caixaInsercaoNome.Text, caixaInsercaoCPF.Text, Convert.ToInt32(escolherCurso.SelectedIndex), caixaInsercaoEmail.Text, caixaInsercaoEndereco.Text, caixaInsercaoTelefone.Text, Convert.ToDouble(caixaInsercaoNotaBim1.Text), Convert.ToDouble(caixaInsercaoNotaBim2.Text), Convert.ToDouble(caixaInsercaoNotaBim3.Text), Convert.ToDouble(caixaInsercaoNotaBim4.Text));
+            aluno.Deletar();
+        }
+
+        private void FormCadastrarAluno_Load(object sender, EventArgs e)
+        {
+            string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=cadastro_escolar_2;";
+            string query = "SELECT nome_curso, codigo_curso FROM curso";
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection)
+            {
+                CommandTimeout = 60
+            };
+            MySqlDataReader reader;
+            try
+            {
+                databaseConnection.Open();
+                reader = commandDatabase.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    escolherCurso.Items.Clear();
+                    while (reader.Read())
+                    {
+                        string[] row = { reader.GetString(0), reader.GetString(1) };
+                        escolherCurso.Items.Add(row[0]);
+                        escolherCurso.Items.IndexOf(row[1]);
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Não existem registros no banco");
+                }
+                databaseConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
